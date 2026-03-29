66b9051ee8041c1cd6344c3c3d7b7cbecb389bcf
Arcelino Cavalcante <arc@MacBook-Air-de-Arcelino.local>
2026-03-29 17:39:07 -0300

    Fix Admin and Student Live Classes button styling

--- assets/Card-ad1b17d6.js
+++
@@ -1,1 +0,0 @@
-import{j as d}from"./index-f60e732a.js";const s=({children:r,className:a=""})=>d.jsx("div",{className:`bg-white dark:bg-gray-800 rounded-xl border border-gray-100 dark:border-gray-700 shadow-sm ${a}`,children:r});export{s as C};
