ae8e8f35544a1b3e7e1361000a42c672a9f6fa63
Arcelino Cavalcante <arc@MacBook-Air-de-Arcelino.local>
2026-03-29 17:17:24 -0300

    Deploy to GitHub Pages

---
+++ assets/AppLayout-d7879c99.js
@@ -0,0 +1,1 @@
+import{a as r,j as e}from"./index-f60e732a.js";const a=({children:s})=>{const{isDarkMode:o}=r();return e.jsx("div",{className:`${o?"dark":""} h-screen flex transition-colors duration-300`,children:s})};export{a as AppLayout};
--- dist/assets/AppLayout-ba4f1e05.js
+++
@@ -1,1 +0,0 @@
-import{a as r,j as e}from"./index-a8d48946.js";const a=({children:s})=>{const{isDarkMode:o}=r();return e.jsx("div",{className:`${o?"dark":""} h-screen flex transition-colors duration-300`,children:s})};export{a as AppLayout};
